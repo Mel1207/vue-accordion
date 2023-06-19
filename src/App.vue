cf06b3ae6dbc0ba95c893328b7fb191cae382194
Mel <meljohnpualon@gmail.com>
2023-06-19 15:40:33 +0800

    added assets

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="section-row">
+      <div class="left-col">
+        <div :class="cardImage.imgClass" v-for="cardImage in cardImages" :key="cardImage.id">
+          
+        </div>
+      </div>
+      <div class="right-col">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At fugiat perferendis repellendus dolores inventore minus vitae maiores deserunt officia deleniti distinctio nesciunt harum nostrum adipisci laborum magnam eligendi mollitia aliquid, pariatur necessitatibus ex impedit! Iure dolore at fugiat voluptates aliquid, ipsam quod ratione, et reiciendis praesentium quos accusantium cum beatae.
+      </div>
+    </div>  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    const cardImages = [
+      { 
+        id: 1,
+        imgName: 'Exibit 1',
+        imgClass: 'img1'
+      },
+      { 
+        id: 2,
+        imgName: 'Exibit 2',
+        imgClass: 'img2'
+      },
+      { 
+        id: 3,
+        imgName: 'Exibit 3',
+        imgClass: 'img3'
+      }
+    ]
+
+    console.log(cardImages.imgSrc)
+    return {
+      cardImages
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
