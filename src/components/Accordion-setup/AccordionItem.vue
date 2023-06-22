599ee7f68e4c1c6e0ccf6b6e3192f74dcbce25ad
Mel <meljohnpualon@gmail.com>
2023-06-22 16:20:44 +0800

    added new adjustments for content and font stylings

--- src/components/Accordion-setup/AccordionItem.vue
+++ src/components/Accordion-setup/AccordionItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="acc-wrapper" v-for="item in accItems" :key="item.id">
     <div class="acc-title" @click="toggleAcc(item)">
-      <p>What is company vission ?</p>
+      <p>{{ item.accTitle }}</p>
 
       <div>
         <img src="../../assets/icon-acc-close.svg" alt="icon close" v-if="item.isOpen">
@@ -25,32 +25,32 @@ export default {
     const accItems = ref([
       {
         id: 1,
-        accTitle: 'Title 1',
+        accTitle: 'How to apply for sponsorship?',
         accContent: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy.',
         isOpen: true
       },
       {
         id: 1,
-        accTitle: 'Title 1',
-        accContent: 'lorem ipsum dolor sit amet',
+        accTitle: 'What are events requirements?',
+        accContent: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, galley of type and scrambled it to make a type specimen book.',
         isOpen: false
       },
       {
         id: 1,
-        accTitle: 'Title 1',
-        accContent: 'lorem ipsum dolor sit amet',
+        accTitle: 'What are the payment modes accepted?',
+        accContent: 'lorem ipsum dolor sit amet Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  ever since the 1500s book.',
         isOpen: false
       },
       {
         id: 1,
-        accTitle: 'Title 1',
-        accContent: 'lorem ipsum dolor sit amet',
+        accTitle: 'What are perks of joining the program?',
+        accContent: 'It is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the dummy text ever since the 1500s, when an unknown printer took a galley.',
         isOpen: false
       },
       {
         id: 1,
-        accTitle: 'Title 1',
-        accContent: 'lorem ipsum dolor sit amet',
+        accTitle: 'Can I get a reference discount for invites?',
+        accContent: 'lorem ipsum dolor sit amet text of the printing and typesetting industry. Lorem Ipsum has been the dummy.',
         isOpen: false
       },
     ])
